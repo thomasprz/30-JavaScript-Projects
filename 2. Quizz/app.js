71a29da814ea73dc7bc14b208a355f1f24df20bd
Thomas <przegralek.thomas@gmail.com>
2025-05-10 10:24:51 +0200

    JS : logique de validation du quiz

--- 2. Quizz/app.js
+++ 2. Quizz/app.js
@@ -1,2 +1,21 @@
 const responses = ["c", "a", "b", "a", "c"];
 const emojis = ["✔️", "✨", "👀", "😭", "👎"];
+
+const form = document.querySelector("form");
+form.addEventListener('button', handleForm)
+
+function handleForm(e){
+    e.preventDefault();
+    quizz();
+}
+
+const inputs = document.querySelectorAll('input');
+
+function getCheckedAnswer(name){
+    const checkbox = document.querySelectorAll('input');
+    checkbox.forEach(cb => {
+        if(cb.checked){
+            const value = cb.value;
+        }
+    })
+}
